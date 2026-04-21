98f038b1aa1774c662bd5957015ce534665723c3
mo7amedmengasu <mohamedmohamed2006teto@gmail.com>
2026-04-21 20:54:24 +0200

    created the files needed for the agent

---
+++ app/my_agent/states/state.py
@@ -0,0 +1,13 @@
+from typing import TypedDict
+
+class MainState(TypedDict):
+    user_message: str
+    intent: str | None
+    response: str | None
+    session_id: str | None
+    customer_id: int | None
+    order_id: int | None
+    extracted_order: dict | None
+    extracted_complaint: dict | None
+    tool_result: dict | None
+    messages: list
